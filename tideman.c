c62358b1e372572a1a8a50a294593b55d1205b07
DxMorganXd <69359492+DxMorganXd@users.noreply.github.com>
2023-12-22 21:51:35 +0100

    Update tideman.c

--- tideman.c
+++ tideman.c
@@ -130,13 +130,13 @@ void add_pairs(void)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            if (preferences[i][j] > preferences[j][i])  // Fix this condition
+            if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
                 pair_count++;
             }
-            else if (preferences[i][j] < preferences[j][i])  // Add this condition
+            else if (preferences[i][j] < preferences[j][i])
             {
                 pairs[pair_count].winner = j;
                 pairs[pair_count].loser = i;
